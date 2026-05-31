e35e58556ff7d78540fd64a070079913ef92fde7
Yash Agrawal <yashagarwal.ya62@gmail.com>
2026-05-31 16:32:43 +0530

    update resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/Yash's Resume New1.pdf",
+      href: "/Yash_Agrawal_updated_resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
